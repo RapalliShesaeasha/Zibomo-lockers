5b7fabc7ae6a71916374f8e849f79b4cae7241bf
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-09-21 13:13:31 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import { sendOTP, verifyOTP, selectShipment, getUserMobile } from '../controllers/authController.js';
+import { sendOTP, verifyOTP, selectShipment } from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/send-otp', sendOTP);
 router.post('/verify-otp', verifyOTP);
 router.post('/select-shipment', selectShipment); // New route for selecting shipment type
-router.post('/get-user-mobile', getUserMobile);
 
 
 
