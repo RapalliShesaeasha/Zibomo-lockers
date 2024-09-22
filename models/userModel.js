11d885c5a3af434a805583dd7a367affcc54820e
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-09-22 11:20:56 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -6,6 +6,8 @@ const userSchema = new mongoose.Schema({
   isVerified: { type: Boolean, default: false },
   shipmentType: { type: String, enum: ['pickup', 'drop'], default: null }, // New field
   receiverMobile: { type: String }, // New field for receiver mobile number
+  lockerSize: { type: String, enum: ['MEDIUM 5X5', 'LARGE 7X7'], default: null }, // New field
+
 
 });
 
