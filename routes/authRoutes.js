a7988bdcb335e964d03f9d1b4feb6021fc184770
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-09-22 11:20:01 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
-import { sendOTP, verifyOTP, selectShipment } from '../controllers/authController.js';
+import { sendOTP, verifyOTP, selectShipment, saveLockerSize, fetchLockerDetails} from '../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/send-otp', sendOTP);
 router.post('/verify-otp', verifyOTP);
 router.post('/select-shipment', selectShipment); // New route for selecting shipment type
+router.post('/save-locker-size', saveLockerSize); // New route for saving locker size
+router.post('/fetch-locker-details', fetchLockerDetails); // New route for fetching locker details
 
 
 
