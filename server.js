ff21fe1fbf516a8ab6ea52cd04b11314ef6bda05
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-10-07 09:29:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import User from './models/userModel.js';  // Make sure this is correctly imported
 import bodyParser from 'body-parser';
+import crypto from 'crypto'; 
 
 dotenv.config();
 connectDB();
