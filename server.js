7e179e979945565de24c25f1bada0a73024e99a0
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-10-07 22:26:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ import User from './models/userModel.js';  // Make sure this is correctly import
 import bodyParser from 'body-parser';
 import authRoutes from './routes/authRoutes.js';  // Make sure this is correctly imported
 import mongoose from 'mongoose';  // Import mongoose here
-import crypto from 'crypto';
-
+import crypto from 'crypto'
 
 dotenv.config();
 connectDB();
@@ -15,10 +14,9 @@ connectDB();
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
 app.use('/api/auth', authRoutes);
-
 // Test route
+
 app.get('/api/test', (req, res) => {
   res.status(200).json({ message: 'The server is running and this is a test route!' });
 });
@@ -26,25 +24,20 @@ app.get('/api/test', (req, res) => {
 // New route to receive transactionId and payment status
 app.post('/payment-status', async (req, res) => {
   const { transactionId, transactionOrderId, paymentStatus } = req.body;
-
   if (!transactionId || !transactionOrderId || !paymentStatus) {
     console.log("Invalid data received: ", req.body);
     return res.status(400).json({ message: 'Invalid data received' });
   }
-
   console.log(`Received payment status update: Transaction ID - ${transactionId}, Order ID - ${transactionOrderId}, Status - ${paymentStatus}`);
-
   try {
     // Use mongoose to validate and convert transactionOrderId to ObjectId
     const objectId = new mongoose.Types.ObjectId(transactionOrderId);
-
     // Find the user and update the paymentStatus
     const user = await User.findOneAndUpdate(
       { _id: objectId },  // Use _id for MongoDB matching
       { paymentStatus: paymentStatus },  // Update the paymentStatus
       { new: true }  // Return the updated user
     );
-
     if (user) {
       console.log('Transaction status updated for user:', user);
       return res.status(200).json({ message: 'Payment status updated successfully' });
@@ -60,24 +53,20 @@ app.post('/payment-status', async (req, res) => {
 });
 
 app.post('/payment-response-v1', async (req, res) => {
-
     // Assuming you are sending a JSON body from main.js
     const paymentResponse = req.body;
-
-    console.log(req.headers);
 
     if (!paymentResponse || !paymentResponse.legalEntityCode || !paymentResponse.orderId) {
         return res.status(400).send('Invalid payment data received');
     }
 
     // Log the data received from main.js
-    //console.log('Data received from main.js:', paymentResponse);
 
-    const boxpaySaltKey = "kBTvCI4t5SkBTvCI4t5T";
-
+    const boxpaySaltKey = process.env.SALT_KEY;
+    
     // Extract the 'x-signature' header
     const receivedSignature = req.headers['x-signature'];
-
+    
     // Construct the signature text
     const signatureText = [
         boxpaySaltKey,
@@ -91,67 +80,68 @@ app.post('/payment-response-v1', async (req, res) => {
         paymentResponse.money.currencyCode,
         paymentResponse.money.amount
     ].join('');
-
+    
     // Hash the signature text using SHA-256
     const hash = crypto.createHash('sha256');
     const output = hash.update(signatureText, 'utf8').digest();
-
+    
     // Convert the output to a hexadecimal string
     let hashText = output.toString('hex');
-
+    
     // Add preceding 0s to make it 64 characters long (if necessary)
     while (hashText.length < 64) {
         hashText = '0' + hashText;
     }
-
+    
     // Calculated signature
     const calculatedSignature = hashText;
-
+    
     // Log the received signature and the calculated signature for comparison
     console.log('Received Signature:', receivedSignature);
     console.log('Calculated Signature:', calculatedSignature);
-
+    
     // Verify if the signature matches
     if (receivedSignature !== calculatedSignature) {
+        console.log('Signature did not match')
         return res.status(403).send('Invalid signature')
     }
-
+    
     // Store paymentResponse in MongoDB
-  const transactionId = paymentResponse.transactionId
-        const transactionOrderId = paymentResponse.orderId  // Send orderId as transactionOrderId
-        const paymentStatus = paymentResponse.status?.status || 'Unknown'
-  
-  if (!transactionId || !transactionOrderId || !paymentStatus) {
-    console.log("Invalid data received: ", req.body);
-    return res.status(400).json({ message: 'Invalid data received' });
-  }
-
-  console.log(`Received payment status update: Transaction ID - ${transactionId}, Order ID - ${transactionOrderId}, Status - ${paymentStatus}`);
-
-  try {
-    // Use mongoose to validate and convert transactionOrderId to ObjectId
-    const objectId = new mongoose.Types.ObjectId(transactionOrderId);
-
-    // Find the user and update the paymentStatus
-    const user = await User.findOneAndUpdate(
-      { _id: objectId },  // Use _id for MongoDB matching
-      { paymentStatus: paymentStatus },  // Update the paymentStatus
-      { new: true }  // Return the updated user
-    );
-
-    if (user) {
-      console.log('Transaction status updated for user:', user);
-    } else {
-      console.log('User not found for the given transactionOrderId.');
+    const transactionId = paymentResponse.transactionId
+    const transactionOrderId = paymentResponse.orderId  // Send orderId as transactionOrderId
+    const paymentStatus = paymentResponse.status?.status || 'Unknown'
+    
+    if (!transactionId || !transactionOrderId || !paymentStatus) {
+        console.log("Invalid data received: ", req.body);
+        return res.status(400).json({ message: 'Invalid data received' });
     }
-  } catch (error) {
-    // Log detailed error
-    console.error('Error updating payment status:', error);
-   
-  }
+
+    console.log(`Received payment status update: Transaction ID - ${ transactionId }, Order ID - ${ transactionOrderId }, Status - ${ paymentStatus }`);
+    
+    try {
+        // Use mongoose to validate and convert transactionOrderId to ObjectId
+        const objectId = new mongoose.Types.ObjectId(transactionOrderId);
+        // Find the user and update the paymentStatus
+        const user = await User.findOneAndUpdate(
+          { _id: objectId },  // Use _id for MongoDB matching
+          { paymentStatus: paymentStatus },  // Update the paymentStatus
+          { new: true }  // Return the updated user
+        );
+        
+        if (user) {
+          console.log('Transaction status updated for user:', user);
+          return res.status(200).json({ message: 'Payment status updated successfully' });
+        } else {
+          console.log('User not found for the given transactionOrderId.');
+          return res.status(404).json({ message: 'User not found.' });
+        }
+
+      } catch (error) {
+        // Log detailed error
+        console.error('Error updating payment status:', error);
+        return res.status(500).json({ message: 'Error saving payment status', error: error.message });
+      }
 });
-
-
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
