8562ffc52f611bb71904e18c756b0b601450ceed
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-10-08 12:23:31 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,19 +1,9 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  mobile: { type: String, required: true, unique: true },
-  otp: { type: String, required: false },
-  isVerified: { type: Boolean, default: false },
-  shipmentType: { type: String, enum: ['pickup', 'drop'], default: null }, // New field
-  receiverMobile: { type: String }, // New field for receiver mobile number
-  lockerSize: { type: String, enum: ['MEDIUM 5X5', 'LARGE 7X7'], default: null }, // New field
-  lockerPrice: { type: Number, default: null }, // New field to store locker price
-  transactionId: { type: String }, // New field for transaction ID
-  transactionOrderId: { type: String },
-  paymentStatus: { type: String }, // New field for payment status
-
-
-
+  name: { type: String, required: true }, // User's name
+  phone: { type: String, required: true }, // User's phone number
+  email: { type: String, required: false }, // Optional email field
 });
 
 const User = mongoose.model('User', userSchema);
