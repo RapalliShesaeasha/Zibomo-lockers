8beb255c444f435a0731caf282ce161b3978d736
Rapalli Shesaeasha <137075837+RapalliShesaeasha@users.noreply.github.com>
2024-10-08 12:44:45 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true }, // User's name
-  mobileNumber: { type: String, required: true }, // User's phone number
+  phone: { type: String, required: true }, // User's phone number
   email: { type: String, required: false }, // Optional email field
 });
 
